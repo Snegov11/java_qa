27a30c687b86d9c9aa9a60b2ac58a5241dcd39ca
Sergei <djfatal@mail.ru>
2022-11-09 08:47:50 +0300

    Классы, атрибуты, конструктор

--- SendBox/src/main/java/ru/java_qa/sendbox/FirstProg.java
+++ SendBox/src/main/java/ru/java_qa/sendbox/FirstProg.java
@@ -1,7 +1,23 @@
 package ru.java_qa.sendbox;
 
 public class FirstProg {
+
+    static Square s = new Square(5);
+    static Rectangle r = new Rectangle(10,3);
+    public static double area(Square s) {
+        return s.l * s.l;
+    }
+    public static double area(Rectangle r) {
+        return r.a * r.b;
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Done!");
+
+        System.out.println(area(s));
+
+        System.out.println(area(r));
+
     }
 }
---
+++ SendBox/src/main/java/ru/java_qa/sendbox/Square.java
@@ -0,0 +1,9 @@
+package ru.java_qa.sendbox;
+
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+}
