a8f034de0f1c671b06ff932f200b7ee55bcac2b9
Sergei <djfatal@mail.ru>
2022-11-11 08:20:21 +0300

    refactoring

--- SeleniumWebTests/src/test/java/org/java/qa/selenium/AddressBookTests.java
+++ SeleniumWebTests/src/test/java/org/java/qa/selenium/AddressBookTests.java
@@ -15,32 +15,59 @@ public class AddressBookTests {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        login("admin", "secret");
+    }
+
+    private void login(String userName, String password) {
+        driver.get("http://localhost/addressbook/");
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(userName);
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
     }
 
     @Test
-    public void addGroupTest() throws Exception {
-        driver.get("http://localhost/addressbook/");
-        driver.findElement(By.name("user")).clear();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).clear();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.xpath("//input[@type='submit']")).click();
-        driver.findElement(By.xpath("//li[@class='admin']")).click();
-        driver.findElement(By.name("new")).click();
+    public void addGroup() throws Exception {
+        goToGroupPage();
+        createNewGroup();
+        fillGroup(new GroupData("Test2", "Test2", "Test2"));
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    private void submitGroupCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroup(GroupData groupData) {
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).clear();
-        driver.findElement(By.name("group_name")).sendKeys("Test2");
+        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
         driver.findElement(By.name("group_parent_id")).click();
         driver.findElement(By.name("group_parent_id")).click();
         driver.findElement(By.name("group_header")).click();
         driver.findElement(By.name("group_header")).clear();
-        driver.findElement(By.name("group_header")).sendKeys("Test2");
+        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
         driver.findElement(By.name("group_footer")).click();
         driver.findElement(By.name("group_footer")).clear();
-        driver.findElement(By.name("group_footer")).sendKeys("Test2");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.xpath("//li[@class='admin']")).click();
-        driver.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void createNewGroup() {
+        driver.findElement(By.name("new")).click();
+    }
+
+    private void goToGroupPage() {
+        goToGroupPage(By.xpath("//li[@class='admin']"));
+    }
+
+    private void returnToGroupPage() {
+        goToGroupPage(By.xpath("//li[@class='admin']"));
+    }
+
+    private void goToGroupPage(By xpath) {
+        driver.findElement(xpath).click();
     }
 
     @AfterClass(alwaysRun = true)
