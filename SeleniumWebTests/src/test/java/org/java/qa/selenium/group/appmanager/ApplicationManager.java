7a88a19dc9abae0f7f879d16f117a973977f81be
Sergei <djfatal@mail.ru>
2022-11-21 09:17:50 +0300

    browsers data for tests

--- SeleniumWebTests/src/test/java/org/java/qa/selenium/group/appmanager/ApplicationManager.java
+++ SeleniumWebTests/src/test/java/org/java/qa/selenium/group/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package org.java.qa.selenium.group.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
@@ -11,10 +14,21 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactsHelper contactsHelper;
+    private Browser browser;
     public WebDriver driver;
 
+    public ApplicationManager(Browser browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        driver = new FirefoxDriver();
+        if (browser == Browser.FIREFOX) {
+            driver = new FirefoxDriver();
+        } else if (browser == Browser.CHROME) {
+            driver = new ChromeDriver();
+        } else if (browser == Browser.EDGE) {
+            driver = new EdgeDriver();
+        }
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://localhost/addressbook/");
         sessionHelper = new SessionHelper(driver);
--- SeleniumWebTests/src/test/java/org/java/qa/selenium/group/tests/TestBase.java
+++ SeleniumWebTests/src/test/java/org/java/qa/selenium/group/tests/TestBase.java
@@ -1,12 +1,13 @@
 package org.java.qa.selenium.group.tests;
 
 import org.java.qa.selenium.group.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.Browser;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(Browser.EDGE);
 
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
